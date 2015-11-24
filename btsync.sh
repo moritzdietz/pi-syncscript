e9165e1c14a7c9f47b73d86867d36c42bcd8bc85
Moritz Dietz <moritz.online@me.com>
2015-11-24 01:55:35 +0100

    v1.1.1
    Minor additions and verbose output tweaking

--- btsync.sh
+++ btsync.sh
@@ -22,11 +22,11 @@ fi
 
 function stop_sync {
   echo "[$stat_y]   Trying to stop a running BitTorrent Sync instance"
-  sleep 0.6
+  sleep 0.5
   syncpid="$(ps aux | grep btsync | grep -v grep | grep -v /bin/bash | awk '{print $2}')"
   if [ -z $syncpid ]; then
     echo "[$stat_ok]  There is no instance of BitTorrent Sync running   "
-    sleep 0.6
+    sleep 0.5
   else
     err_cmd=$(sudo pkill -15 -x btsync 2>&1 >/dev/null)
     if [ $? -ne 0 ]; then
@@ -34,11 +34,11 @@ function stop_sync {
       if [ ! $err_cmd=="" ]; then
         echo "[$stat_x]   $err_cmd"
       fi
-      sleep 0.6
+      sleep 0.5
       exit 1
     else
       echo "[$stat_ok]  The running instance of BitTorrent Sync has been stopped (PID: $syncpid)"
-      sleep 0.6
+      sleep 0.5
     fi
   fi
 }
@@ -46,7 +46,7 @@ function stop_sync {
 function install_preperations {
   if [ ! -d "$btsdir" ]; then
     echo "[$stat_ok]  Trying to create installation folder $btsdir"
-    sleep 0.6
+    sleep 0.5
     err_cmd=$(mkdir $btsdir 2>&1 >/dev/null)
       if [ $? -ne 0 ]; then
         echo "[$stat_x]   Could not create installation folder $btsdir"
@@ -54,10 +54,10 @@ function install_preperations {
         exit 1
       fi
     echo "[$stat_ok]  BitTorrent Sync installation folder has been created ($btsdir)"
-    sleep 0.6
+    sleep 0.5
   else
     echo "[$stat_x]   BitTorrent Sync installation folder $btsdir already exists"
-    sleep 0.6
+    sleep 0.5
     read -r -p "[$stat_x]   Delete all files and sub-folders inside $btsdir? [Y/N]: " response
     if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
         echo -ne "[$stat_y]   Deleting all files in $btsdir\r"
@@ -71,13 +71,13 @@ function install_preperations {
     fi
       if [ -f $btsdir/btsync_arm.tar.gz ]; then
           echo "[$stat_y]   A binary file already exists"
-          sleep 0.6
+          sleep 0.5
           echo "[$stat_ok]  Trying to remove and re-download the latest binary"
           err_cmd=$(rm $btsdir/btsync_arm.tar.gz 2>&1 >/dev/null)
           if [ $? -ne 0 ]; then
             echo "[$stat_x]   Error: Could not remove $btsdir/btsync_arm.tar.gz"
             echo "[$stat_x]   $err_cmd"
-            sleep 0.6
+            sleep 0.5
             exit 1
           fi
       fi
@@ -85,7 +85,7 @@ function install_preperations {
 }
 
 function install {
-  sleep 0.6
+  sleep 0.5
   err_cmd=$(cd $btsdir 2>&1 >/dev/null)
   if [ $? -ne 0 ]; then
       echo "[$stat_x]   Error: Could not change to $btsdir since it does not exist"
@@ -93,26 +93,26 @@ function install {
       exit 1
   fi
   if [ -z $dllink ]; then
-      echo "[$stat_ok]  Downloading the latest stable version from BitTorrent Inc"
+      echo "[$stat_y]   Downloading the latest stable version from BitTorrent Inc"
       curl -# -o $btsdir/btsync_arm.tar.gz https://download-cdn.getsync.com/stable/linux-arm/BitTorrent-Sync_arm.tar.gz
       if [ $? -ne 0 ]; then
         echo "[$stat_x]   Error: There was an error downloading the BiTorrent Sync binary"
         exit 1
       else
-        echo "[$stat_ok]  Successfully downloaded the BiTorrent Sync binary"
+        echo -en "\e[1A"; echo -e "\e[0K\r[$stat_ok]  Successfully downloaded the binary"
       fi
     else 
-      echo "[$stat_ok]  Downloading the binary from the link provided"
+      echo "[$stat_y]   Downloading the binary from the link provided"
       curl -# -o $btsdir/btsync_arm.tar.gz $dllink
       if [ $? -ne 0 ]; then
         echo "[$stat_x]   Error: There was an error downloading the file using the link you provided. Please check the URL and try again"
         exit 1
       else
-        echo "[$stat_ok]  Successfully downloaded the BiTorrent Sync binary"
+        echo -en "\e[1A"; echo -e "\e[0K\r[$stat_ok]  Successfully downloaded the binary"
       fi
   fi
   if [ $? -ne 0 ]; then
-      echo -ne "[$stat_x]   Error: There was an error downloading the BiTorrent Sync binary\r"
+      echo -ne "[$stat_x]   Error: There was an error downloading the binary"
       echo -ne '\n'
       exit 1
   fi
@@ -122,7 +122,7 @@ function install {
       echo "[$stat_x]   $err_cmd"
       exit 1
   fi
-  echo "[$stat_ok]  Extraced the BitTorrent Sync binary to /usr/bin/btsync"
+  echo "[$stat_ok]  Extraced the binary to /usr/bin/btsync"
   err_cmd=$(rm $btsdir/btsync_arm.tar.gz 2>&1 >/dev/null)
   if [ $? -ne 0 ]; then
       echo "[$stat_x]   Error: Could not remove $btsdir/btsync_arm.tar.gz"
@@ -137,9 +137,9 @@ function btsync_initscript {
   else
     err_cmd=$(curl -o $btsdir/btsync_init https://raw.githubusercontent.com/moritzdietz/pi-syncscript/master/configuration-files/btsync 2>&1 >/dev/null)
     if [ $? -ne 0 ]; then
-        echo "[$stat_x]   Error: There was an error downloading the BitTorrent Sync init.d script"
+        echo "[$stat_x]   Error: There was an error downloading the init.d script"
         echo "[$stat_x]   $err_cmd"
-        sleep 0.6
+        sleep 0.5
         exit 1
       else
         sed -r -i.tmp "s/\bpi\b/$user/g" $btsdir/btsync_init
@@ -154,9 +154,9 @@ function btsyncconfig {
   if [ ! -f /etc/btsync/config.json ]; then
     err_cmd=$(curl -o $btsdir/config.json https://raw.githubusercontent.com/moritzdietz/pi-syncscript/master/configuration-files/config.json 2>&1 >/dev/null)
       if [ $? -ne 0 ]; then
-        echo "[$stat_x]   Error: There was an error downloading the BitTorrent Sync configuration file"
+        echo "[$stat_x]   Error: There was an error downloading the configuration file"
         echo "[$stat_x]   $err_cmd"
-        sleep 0.6
+        sleep 0.5
         exit 1
       else
         sed -r -i.tmp "s/\bpi\b/$user/g" $btsdir/config.json
@@ -173,7 +173,7 @@ function btsyncconfig {
 
 function backup {
   backup_date=$(date +"%d-%m-%Y_%H-%M-%S")
-  backup_reason="The BitTorrent Sync is being backed up as (/home/$user/btsync_backup_$backup_date.tar.gz)"
+  backup_reason="BitTorrent Sync is being backed up"
   # Look for the 4 common signals that indicate this script was killed.
   # If the background command was started, kill it, too.
   if [ -e ${btsdir} ]; then
@@ -197,7 +197,7 @@ function backup {
     echo -ne "[Ooo] ${backup_reason}\r" && sleep 0.2
     echo -ne "[ooo] ${backup_reason}\r"
   done
-  echo -e "\e[0K\r[$stat_ok]  BitTorrent Sync has been successfully backed up"  
+  echo -e "\e[0K\r[$stat_ok]  BitTorrent Sync has been successfully backed up at /home/$user/btsync_backup_$backup_date.tar.gz"
 }
 
 function remove {
@@ -230,14 +230,20 @@ function remove {
     fi
 }
 
+function version_check {
+  if [ -f /usr/bin/btsync ]; then
+    echo $(/usr/bin/btsync -help | grep "BitTorrent Sync" | awk {'print $3" "$4'})
+  fi
+}
+
 read -r -p "[$stat_y]   Choose one of the BitTorrent Sync Script options [(i)nstall(default)/(u)pdate/(b)ackup/(r)emove]: " response
 if [[ $response =~ ^([u|U]|[u|U]pdate)$ ]]; then
   stop_sync
-  echo  "[$stat_ok]  Updating BitTorrent Sync from version" $(/usr/bin/btsync -help | grep "BitTorrent Sync" | awk {'print $3" "$4'}) "to the latest available version"
+  echo  "[$stat_ok]  Updating BitTorrent Sync from version $(version_check) to the latest available version"
   install $dllink
   btsync_initscript
   btsyncconfig
-  echo "[$stat_ok]  Updated BitTorrent Sync to version" $(/usr/bin/btsync -help | grep "BitTorrent Sync" | awk {'print $3" "$4'})
+  echo "[$stat_ok]  Updated BitTorrent Sync to version $(version_check)"
   echo "[$stat_ok]  You can now start BitTorrent Sync by typing \"sudo service btsync start\""
   exit 0
 elif [[ $response =~ ^([b|B]|[b|B]ackup)$ ]]; then
@@ -250,7 +256,7 @@ elif [[ $response =~ ^(""|[i|I]|[i|I]nstall)$ ]]; then
   install
   btsync_initscript
   btsyncconfig
-  echo "[$stat_ok]  Installed BitTorrent Sync version" $(/usr/bin/btsync -help | grep "BitTorrent Sync" | awk {'print $3" "$4'})
+  echo "[$stat_ok]  Installed BitTorrent Sync version $(version_check)"
   echo "[$stat_ok]  You can now start BitTorrent Sync by typing \"sudo service btsync start\""
   exit 0
 elif [[ $response =~ ^(""|[r|R]|[r|R]emove)$ ]]; then
